df2d1fd1e0c3f50e847acf157ec2d67cb40ed921
felipe <silas.garcia@mfconsulting.com.br>
2020-07-24 00:47:11 -0300

    utilizando koin para injeção de dependencia

--- app/src/main/java/br/com/alura/technews/di/AppModules.kt
+++ app/src/main/java/br/com/alura/technews/di/AppModules.kt
@@ -2,6 +2,13 @@ package br.com.alura.technews.di
 
 import androidx.room.Room
 import br.com.alura.technews.database.AppDatabase
+import br.com.alura.technews.database.dao.NoticiaDAO
+import br.com.alura.technews.repository.NoticiaRepository
+import br.com.alura.technews.retrofit.webclient.NoticiaWebClient
+import br.com.alura.technews.ui.viewmodel.FormularioNoticiaViewModel
+import br.com.alura.technews.ui.viewmodel.ListaNoticiasViewModel
+import br.com.alura.technews.ui.viewmodel.VisualizaNoticiaViewModel
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 
@@ -15,6 +22,30 @@ val appModules = module {
             AppDatabase::class.java,
             NOME_BANCO_DE_DADOS
         ).build()
+    }
+
+    single<NoticiaDAO> {
+        get<AppDatabase>().noticiaDAO
+    }
+
+    single<NoticiaWebClient> {
+        NoticiaWebClient()
+    }
+
+    single<NoticiaRepository> {
+        NoticiaRepository(get(), get())
+    }
+
+    viewModel<ListaNoticiasViewModel> {
+        ListaNoticiasViewModel(get())
+    }
+
+    viewModel<VisualizaNoticiaViewModel> { (id: Long) ->
+        VisualizaNoticiaViewModel(id,get())
+    }
+
+    viewModel<FormularioNoticiaViewModel> {
+        FormularioNoticiaViewModel(get())
     }
 
 }
--- app/src/main/java/br/com/alura/technews/ui/activity/VisualizaNoticiaActivity.kt
+++ app/src/main/java/br/com/alura/technews/ui/activity/VisualizaNoticiaActivity.kt
@@ -6,17 +6,13 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import br.com.alura.technews.R
-import br.com.alura.technews.database.AppDatabase
 import br.com.alura.technews.model.Noticia
-import br.com.alura.technews.repository.NoticiaRepository
 import br.com.alura.technews.ui.activity.extensions.mostraErro
 import br.com.alura.technews.ui.viewmodel.VisualizaNoticiaViewModel
-import br.com.alura.technews.ui.viewmodel.factory.FormularioNoticiaViewModelFactory
-import br.com.alura.technews.ui.viewmodel.factory.VisualizaNoticiaViewModelFactory
 import kotlinx.android.synthetic.main.activity_visualiza_noticia.*
-import org.koin.android.ext.android.inject
+import org.koin.android.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
 
 private const val NOTICIA_NAO_ENCONTRADA = "Notícia não encontrada"
 private const val TITULO_APPBAR = "Notícia"
@@ -25,14 +21,8 @@ private const val MENSAGEM_FALHA_REMOCAO = "Não foi possível remover notícia"
 class VisualizaNoticiaActivity : AppCompatActivity() {
 
 
-    private val database by inject<AppDatabase>()
+    private val viewModel: VisualizaNoticiaViewModel by viewModel { parametersOf(noticiaId) }
 
-    private val viewModel by lazy {
-        val repository = NoticiaRepository(database.noticiaDAO)
-        val factory = VisualizaNoticiaViewModelFactory(noticiaId, repository)
-        ViewModelProviders.of(this, factory)
-            .get(VisualizaNoticiaViewModel::class.java)
-    }
 
     private val noticiaId: Long by lazy {
         intent.getLongExtra(NOTICIA_ID_CHAVE, 0)
@@ -105,5 +95,6 @@ class VisualizaNoticiaActivity : AppCompatActivity() {
         intent.putExtra(NOTICIA_ID_CHAVE, noticiaId)
         startActivity(intent)
     }
-
 }
+
+
